af53266bcc90ac8fc37cb233460a01ccaf109de4
Сергей <lockup86@rambler.ru>
2024-12-05 22:06:18 +0300

    HM 29.MVCTest1

--- src/test/java/ru/hogwarts/school/homework29/controller/StudentControllerTest.java
+++ src/test/java/ru/hogwarts/school/homework29/controller/StudentControllerTest.java
@@ -11,10 +11,14 @@ import org.springframework.test.web.servlet.ResultActions;
 import ru.hogwarts.school.homework29.model.Student;
 import ru.hogwarts.school.homework29.service.StudentService;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(StudentController.class)
@@ -31,45 +35,25 @@ class StudentControllerTest {
 
 
     @MockBean
-
     private StudentService studentService;
 
     @Test
 
     void shouldGetStudent() throws Exception {
-
-        // given
-
         Long studentId = 1L;
-
         Student student = new Student("Ivan", 20);
 
-
-
         when(studentService.get(studentId)).thenReturn(student);
-
-
-
-        // when
-
-        ResultActions perform = mockMvc.perform(get("/students/{id}", studentId));
-
-
-
-        // then
+        ResultActions perform = mockMvc.perform(get("/student/{id}", studentId));
 
         perform
-
                 .andExpect(jsonPath("$.name").value(student.getName()))
-
                 .andExpect(jsonPath("$.age").value(student.getAge()))
-
                 .andDo(print());
 
     }
 
     @Test
-
     void shouldCreateStudent() throws Exception {
         Long studentId = 1L;
         Student student = new Student("Ivan", 20);
@@ -87,21 +71,86 @@ class StudentControllerTest {
                 .andDo(print());
     }
 
-    @Test
-
+    /*@Test
     void shouldUpdateStudent() throws Exception {
         Long studentId = 1L;
-        Student student = new Student("Ivan", 20);
+        Student student = new Student("Ivan", 21);
+        Student student1 = new Student("Ivan", 20);
 
-        when(studentService.update(student)).thenReturn(student);
-        ResultActions perform = mockMvc.perform(put("/student/{id}", studentId)
+        when(studentService.create(any(Student.class))).thenReturn(student);
+        when(studentService.update(student)).thenReturn(student1);
+        ResultActions perform = mockMvc.perform(put("/student")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(student)));
 
         perform
-                .andExpect(jsonPath("$.name").value(student.getName()))
-                .andExpect(jsonPath("$.age").value(student.getAge()))
+                .andExpect(jsonPath("$id").value(student1.getId()))
+                .andExpect(jsonPath("$.name").value(student1.getName()))
+                .andExpect(jsonPath("$.age").value(student1.getAge()))
                 .andDo(print());
     }
+
+     */
+
+    @Test
+    void shouldUpdate() throws Exception {
+        Long id = 1L;
+        Student student = new Student("Ivan", 20);
+        Student student1 = new Student("Test", 21);
+
+        student.setId(id);
+        student1.setId(id);
+
+        when(studentService.create(any(Student.class))).thenReturn(student);
+        when(studentService.update(any(Student.class))).thenReturn(student1);
+
+
+        ResultActions perform = mockMvc.perform(put("/student")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(student1)));
+        perform
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.name").value("Test"))
+                .andExpect(jsonPath("$.age").value(21))
+                .andDo(print());
+    }
+
+
+
+
+    @Test
+    void testGetMinMax() throws Exception {
+        List <Student> list = List.of(
+        new Student("Test1", 10),
+        new Student("Test2", 13),
+        new Student("Test3", 17));
+        int min = 10;
+        int max = 13;
+
+
+
+        when(studentService.findBetween(min, max)).thenReturn(list);
+
+        ResultActions perform = mockMvc.perform(get("/student")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(list)));
+        perform
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andDo(print());
+
+    }
+    /*@Test
+    public void givenGreetURIWithQueryParameter_whenMockMVC_thenResponseOK() throws Exception {
+        this.mockMvc.perform(get("/student")
+                        .param("name", "John Doe")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.message").value("Hello World John Doe!!!"));
+    }
+
+     */
+
+
 
 }
